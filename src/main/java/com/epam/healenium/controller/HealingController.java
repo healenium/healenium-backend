7c6cfc075294ae3271a8d3b5793758c6dfdd4987
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2020-09-09 16:10:02 +0300

    Add Prometheus integration for statistics monitoring

--- src/main/java/com/epam/healenium/controller/HealingController.java
+++ src/main/java/com/epam/healenium/controller/HealingController.java
@@ -1,17 +1,30 @@
 package com.epam.healenium.controller;
 
-import com.epam.healenium.model.dto.*;
+import com.epam.healenium.model.dto.HealingDto;
+import com.epam.healenium.model.dto.HealingRequestDto;
+import com.epam.healenium.model.dto.HealingResultDto;
+import com.epam.healenium.model.dto.RequestDto;
+import com.epam.healenium.model.dto.SelectorRequestDto;
 import com.epam.healenium.service.HealingService;
 import com.epam.healenium.treecomparing.Node;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import static com.epam.healenium.constants.Constants.SESSION_KEY;
 
 @Slf4j
 @RestController
@@ -33,6 +46,7 @@ public class HealingController {
 
     /**
      * Getting last valid path for provided request
+     *
      * @param dto
      * @return
      */
@@ -43,20 +57,24 @@ public class HealingController {
 
     /**
      * Saving heal result for specific selector
+     *
      * @param dto
-     * @param sessionId
+     * @param headers
      * @return
      */
     @PostMapping("/healing")
-    public void save(@Valid @RequestParam("dto") HealingRequestDto dto, @RequestParam("screenshot") MultipartFile screenshot, @RequestHeader(value = "sessionKey", required = false) String sessionId) {
-        if (StringUtils.isEmpty(sessionId)) {
+    public void save(@Valid @RequestParam("dto") HealingRequestDto dto,
+                     @RequestParam("screenshot") MultipartFile screenshot,
+                     @RequestHeader Map<String, String> headers) {
+        if (StringUtils.isEmpty(headers.get(SESSION_KEY))) {
             log.warn("Session key is not present. Current issue would not be presented in any reports, but still available in replacement!");
         }
-        healingService.saveHealing(dto, screenshot, sessionId);
+        healingService.saveHealing(dto, screenshot, headers);
     }
 
     /**
      * Getting healing with their results for provided request
+     *
      * @param dto
      * @return
      */
--- src/main/java/com/epam/healenium/util/Utils.java
+++ src/main/java/com/epam/healenium/util/Utils.java
@@ -1,10 +1,22 @@
 package com.epam.healenium.util;
 
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.DigestUtils;
 
+import javax.crypto.SecretKey;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.Base64;
 
 @Slf4j
 @UtilityClass
@@ -33,6 +45,16 @@ public class Utils {
     public String buildHealingKey(String elementId, String pageSource) {
         String rawKey = elementId + pageSource.hashCode();
         return DigestUtils.md5DigestAsHex(rawKey.trim().getBytes(StandardCharsets.UTF_8));
+    }
+
+    @SneakyThrows
+    public String getKeyFromKeyStore(String alias) {
+        KeyStore keyStore = KeyStore.getInstance("JCEKS");
+        InputStream keyStoreFile = new FileInputStream("src/main/resources/keystore/prometrics.ks");
+        char[] keyStorePassword = "uSi51JkQTJlgi".toCharArray();
+        keyStore.load(keyStoreFile, keyStorePassword);
+        SecretKey key = (SecretKey) keyStore.getKey(alias, keyStorePassword);
+        return new String(Base64.getDecoder().decode(key.getEncoded()));
     }
 
 }
