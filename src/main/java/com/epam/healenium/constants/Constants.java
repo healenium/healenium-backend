86d444882db492f6c1f7800994069f93f5de3f71
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2020-10-25 19:04:56 +0300

    Error during open report

--- src/main/java/com/epam/healenium/constants/Constants.java
+++ src/main/java/com/epam/healenium/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String PUSHGATEWAY_SERVER_ADDRESS = "35.158.107.78:9091";
     public static final String PROMETHEUS_JOB_NAME = "healenium_job";
 
-    public static final String SESSION_KEY = "sessionKey";
+    public static final String SESSION_KEY = "sessionkey";
 
 
 }
