66bbcaedeb75dd8324acd9827719058c16348ca3
Aliaksei_Ashukha <aliaksei_ashukha@epam.com>
2020-11-23 11:40:17 +0300

    Fix userFlag issue. Update prometheus URL. Add prometheus metrics.

--- src/main/java/com/epam/healenium/config/PrometheusConfiguration.java
+++ src/main/java/com/epam/healenium/config/PrometheusConfiguration.java
@@ -4,16 +4,18 @@ import com.epam.healenium.util.Utils;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.BasicAuthHttpConnectionFactory;
 import io.prometheus.client.exporter.PushGateway;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusPushGatewayManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.net.URL;
 import java.time.Duration;
 import java.util.Map;
 
 import static com.epam.healenium.constants.Constants.PROMETHEUS_JOB_NAME;
-import static com.epam.healenium.constants.Constants.PUSHGATEWAY_SERVER_ADDRESS;
 
 /**
  * @author rdanilov
@@ -22,14 +24,18 @@ import static com.epam.healenium.constants.Constants.PUSHGATEWAY_SERVER_ADDRESS;
 @Configuration
 public class PrometheusConfiguration {
 
+    @Value("${app.prometheus.url}")
+    private String prometheusURL;
+
     @Bean
     @Scope(value = "prototype")
-    public PrometheusPushGatewayManager prometheusPushGatewayManager(Map<String, String> headers) {
-        PushGateway pg = new PushGateway(PUSHGATEWAY_SERVER_ADDRESS);
+    @SneakyThrows
+    public PrometheusPushGatewayManager prometheusPushGatewayManager(Map<String, String> groupingKeys) {
+        PushGateway pg = new PushGateway(new URL(prometheusURL));
         pg.setConnectionFactory(new BasicAuthHttpConnectionFactory(
                 Utils.getKeyFromKeyStore("loginAlias"),
                 Utils.getKeyFromKeyStore("passwordAlias")));
         return new PrometheusPushGatewayManager(pg, CollectorRegistry.defaultRegistry, Duration.ofSeconds(1L),
-                PROMETHEUS_JOB_NAME, headers, PrometheusPushGatewayManager.ShutdownOperation.PUSH);
+                PROMETHEUS_JOB_NAME, groupingKeys, PrometheusPushGatewayManager.ShutdownOperation.PUSH);
     }
 }
--- src/main/java/com/epam/healenium/constants/Constants.java
+++ src/main/java/com/epam/healenium/constants/Constants.java
@@ -2,10 +2,11 @@ package com.epam.healenium.constants;
 
 public class Constants {
 
-    public static final String PUSHGATEWAY_SERVER_ADDRESS = "35.158.107.78:9091";
     public static final String PROMETHEUS_JOB_NAME = "healenium_job";
-
+    public static final String HEALING_ID = "healing_id";
     public static final String SESSION_KEY = "sessionkey";
+    public static final String INSTANCE = "instance";
+    public static final String HOST_PROJECT = "hostproject";
 
 
 }
